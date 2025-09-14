2715116a222bf1a85c466b87b55df2fc589ba91f
zh46k4 <agapovivan2004@mail.ru>
2025-09-14 15:54:37 +0300

    Add files via upload

---
+++ Лаба 1/Код с выполненными заданиями.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+public struct Student
+{
+    public string LastName;
+    public string FirstName;
+    public string Group;
+    public int Year;
+}
+
+class Program
+{
+    static void Main()
+    {
+       
+        List<Student> students = new List<Student>();
+        students.Add(new Student { LastName = "ВВВВААААА", FirstName = "Влад", Group = "24ВВВ3", Year = 2000 });
+        students.Add(new Student { LastName = "ВВВКУКУКУ", FirstName = "Ваня", Group = "24ВВВ3", Year = 2001 });
+        students.Add(new Student { LastName = "ывываыва", FirstName = "", Group = "24ВВВ3", Year = 2000 });
+
+        
+        Console.WriteLine("Введите фамилию для поиска:");
+        string searchName = Console.ReadLine();
+
+        
+        bool found = false;
+        foreach (Student s in students)
+        {
+            if (s.LastName.Contains(searchName))
+            {
+                Console.WriteLine("Найден: " + s.LastName + " " + s.FirstName + ", группа " + s.Group);
+                found = true;
+            }
+        }
+
+       
+        if (!found)
+        {
+            Console.WriteLine("Студент не найден");
+        }
+
+
+        Random rnd = new Random();
+        Console.WriteLine("Размер массива:");
+         
+        int cols = Convert.ToInt32(Console.ReadLine());
+        int rows = Convert.ToInt32(Console.ReadLine());
+        int[,] matrix = new int[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                matrix[i, j] = rnd.Next(-15, 15);
+            }
+        }
+
+        Console.WriteLine("Двумерный массив:");
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+        int sum = 0;
+        foreach (int s in matrix) { 
+            if(s<=0) sum +=1;
+        }
+        Console.WriteLine("Кол-во: " + sum);
+
+        Console.WriteLine("Размер массива:");
+        int size = Convert.ToInt32(Console.ReadLine());
+        int[,] matrix_v2 = new int[size, size];
+
+       
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = i; j < size; j++)
+            {
+                if (i == j)
+                {
+                    
+                    matrix_v2[i, j] = 1;
+                }
+                else
+                {
+                    
+                    int value = rnd.Next(0, 2);
+                    matrix_v2[i, j] = value;
+                    
+                    matrix_v2[j, i] = value;
+                }
+            }
+        }
+
+        
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                Console.Write(matrix_v2[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+
+            Console.WriteLine("Нажмите Enter для выхода");
+        Console.ReadLine();
+    }
+}
---
+++ Лаба 1/Основной код.cs
@@ -0,0 +1,121 @@
+﻿
+using System;
+using System.Data.Common;
+using System.Xml.Linq;
+
+namespace FirstProgram
+{
+
+    class Program
+    {
+        
+        static int[,] cratematrix(int cols ,int rows, int [,]matrix) {
+
+            Random rnd = new Random();
+            
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    matrix[i, j] = rnd.Next(0, 10);
+                }
+            }
+            return matrix;
+
+        }
+
+        static void PrintMatrix(int[,] matrix, int rows, int cols)
+        {
+            Console.WriteLine("Двумерный массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void Differenceofelements(int[,] matrix) {
+            int max = int.MinValue, min = int.MaxValue;
+            foreach (int i in matrix) {
+                if (i > max) max = i;
+                if (i < min) min = i;
+            }
+            int differense = max - min;
+            Console.WriteLine(differense);
+            
+        }
+
+        static void countbychoice(int[,] matrix , int cols, int rows) {
+
+            Console.WriteLine("Выберите что считать, для ситолбцов введите j, для строк i");
+            string choice = Console.ReadLine();
+            Console.WriteLine("Выберите номер (строки или столбца)");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int sum = 0;
+
+            switch (choice) {
+                case "i":
+                        if (num < rows)
+                        {
+                            for (int i = 0; i < matrix.GetLength(0); i++)
+                            {
+                                for (int j = 0; j < matrix.GetLength(1); j++)
+                                {
+                                    if (i == num) sum += matrix[i, j];
+                                }
+                            }
+                            Console.WriteLine(sum);
+                        }
+                        else Console.WriteLine("Неверный ввод номера столбца");
+                        break;
+                case "j":
+                        if (num < cols)
+                        {
+                            for (int i = 0; i < matrix.GetLength(0); i++)
+                            {
+                                for (int j = 0; j < matrix.GetLength(1); j++)
+                                {
+                                    if (j == num) sum += matrix[i, j];
+                                }
+                            }
+                            Console.WriteLine(sum);
+                        }
+                        else Console.WriteLine("Неверный ввод номера столбца");
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели явно не i и не j");
+                    break;
+            }   
+            
+        }
+            static void Main()
+        {
+            
+
+            Console.WriteLine("Введите размер массива (столбцы)");
+            int cols = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Введите размер массива (строки)");
+            int rows = Convert.ToInt32(Console.ReadLine());
+            
+
+            int[,] matrix = new int[rows, cols];
+
+            cratematrix(cols, rows, matrix);
+            PrintMatrix(matrix, rows, cols);
+
+            Differenceofelements(matrix);
+
+            countbychoice(matrix , cols, rows);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
